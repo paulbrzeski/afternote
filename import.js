f899980e4f09370bdc6c88b787dddb91ab8c4c72
Paul Brzeski <pbrzeski@doghouse.agency>
2017-04-16 22:11:25 +0800

    Able to target inner contents of note without an extra parsing library. Going to now convert HTML to markdown

--- import.js
+++ import.js
@@ -6,16 +6,35 @@ const fs = require('fs'),
       path      = require('path'),
       XmlStream = require('xml-stream');
 
+function formatNoteContent (note) {
+  return note.substring(
+    note.indexOf("<en-note>") + "<en-note>".length,
+    note.indexOf("</en-note>")
+  );
+}
+
 let filename = process.argv[2];
 var stream = fs.createReadStream(filename);
 var xml = new XmlStream(stream);
 xml.on('updateElement: note', function(note) {
-  console.log(note);
+  var mapping = {
+    name: note.title,
+    data: formatNoteContent(note.content),
+    original_created: note.created,
+    original_updated: note.updated
+  }
+  console.log(mapping);
+  for (var keyname in note) {
+    console.log(keyname);
+  }
+  //console.log(note);
   die();
 });
-xml.on('error', function(message) {
-  console.log('Parsing as ' + ('auto') + ' failed: ' + message);
-});
-xml.on('data', function(data) {
-  //process.stdout.write(data);
-});
+
+// // Debug
+// xml.on('error', function(message) {
+//   console.log('Parsing as ' + ('auto') + ' failed: ' + message);
+// });
+// xml.on('data', function(data) {
+//   //process.stdout.write(data);
+// });
