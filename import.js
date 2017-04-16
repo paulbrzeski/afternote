ffc1ae724745400043e9fb1af68828523fe55694
Paul Brzeski <pbrzeski@doghouse.agency>
2017-04-16 23:23:20 +0800

    Looping through images. Ready to start converting resource elements

--- import.js
+++ import.js
@@ -7,24 +7,43 @@ const fs = require('fs'),
       toMarkdown = require('to-markdown'),
       XmlStream = require('xml-stream');
 
-function formatNoteContent (note) {
-  return (note.substring(
-    note.indexOf("<en-note>") + "<en-note>".length,
-    note.indexOf("</en-note>")
+function formatNoteContent (content) {
+  return replaceMediaTags(content.substring(
+    content.indexOf("<en-note>") + "<en-note>".length,
+    content.indexOf("</en-note>")
   ));
+}
+
+function replaceMediaTags (content) {
+  let pattern = /<en-media ([\s\S]*?)><\/en-media>/g;
+  let match = pattern.exec(content);
+  while(match != null) {
+    let [original_element, raw_metadata] = match;
+    raw_metadata = raw_metadata.split(' ');
+    let metadata = {};
+
+    for (var keyname in raw_metadata) {
+      // note both ascii double quotes are here... hail satan.
+      let parsed_metadata = raw_metadata[keyname].replace('"','').replace('"','').split('=');
+      metadata[parsed_metadata[0]] = parsed_metadata[1];
+    }
+    console.log(metadata);
+    match = pattern.exec(content);
+  }
+  return content;
 }
 
 let filename = process.argv[2];
 var stream = fs.createReadStream(filename);
 var xml = new XmlStream(stream);
-xml.on('updateElement: note', function(note) {
+xml.on('endElement: note', function(note) {
   var mapping = {
     name: note.title,
     data: formatNoteContent(note.content),
     original_created: note.created,
     original_updated: note.updated
   }
-  console.log(mapping);
+  //console.log(mapping);
   for (var keyname in note) {
     console.log(keyname);
   }
