62f47be65cb1b41f19c1a022fdbb1a9a81f3ca9e
Paul Brzeski <mail@paulbrzeski.com>
2017-04-18 00:26:24 +0800

    Got list loading. Need to work on UI.. do some wireframes and fillout in Illustrator

--- src/main.js
+++ src/main.js
@@ -2,12 +2,25 @@
 const {ipcRenderer} = require('electron')
 let notebooks = ipcRenderer.sendSync('client-event', 'ready');
 $('<ul/>', {id: 'notebook_list'}).appendTo('#container');
-for (var stacks in notebooks) {
+for (var bookname in notebooks) {
   $('<li/>', {
-  	text: stacks.toUpperCase()
+  	html: buildNoteList(bookname, notebooks[bookname])
   }).appendTo('#notebook_list');
 }
 ipcRenderer.on('asynchronous-reply', (event, arg) => {
   console.log(arg) // prints "pong"
 })
 ipcRenderer.send('asynchronous-message', 'ping')
+
+function buildNoteList (bookname, books) {
+
+  let $list = $("<ul/>", {'data-bookname': bookname});
+  for (var book in books) {
+  	$('<li/>', {
+	  text: book
+	}).appendTo($list);
+  }
+
+  return 	$('<h2/>', {text: bookname.toUpperCase()}).prop('outerHTML') + 
+  			$list.prop('outerHTML')
+}
