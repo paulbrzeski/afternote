77941c5defebc8cf196faff8f06fcb11e8bb67aa
Paul Brzeski <mail@paulbrzeski.com>
2017-05-23 19:57:31 +0800

    Added brainstorm... other updates apparently lol

--- src/main.js
+++ src/main.js
@@ -9,23 +9,22 @@ buildNotebookList(notebooks);
 
 function buildNotebookList (notebooks) {
   let $list = $("<div/>", {
-    class: 'ui relaxed link list inverted'
+    class: 'ui vertical inverted menu overlay visible fixed sticky'
   });
 
   let firstLoaded = false;
   for (var bookname in notebooks) {
     let $link = $('<a/>', {
-      class: 'content',
       'data-bookname': bookname,
       href: '#',
       style: 'text-transform: capitalize',
       html: $('<a/>', {
+        class: 'item',
         html: bookname + '<i class="edit icon right floated"></i>'
       })
     });
     $link.click(clickHandler);
     $list.append($('<div/>', {
-      class: 'item',
       html: $link
     }));
 
@@ -35,6 +34,9 @@ function buildNotebookList (notebooks) {
     }
   }
   $list.appendTo('#notebooks');
+  $('.ui.sticky').sticky({
+    context: '#context'
+  })
 }
 
 function clickHandler(e) {
@@ -66,6 +68,9 @@ function buildNoteList (bookname, books) {
   }
   
   $('#notes').html($list);
+  $('.ui.sticky')
+    .sticky('refresh')
+  ;
 }
 
 function loadNote (filename) {
